3b8541f6a27d5af407f57b993f55d06b51bdfa79
Oleksandr Myrvoda <mirvoda2@gmail.com>
2021-12-11 14:04:43 +0200

    hw03 image-finder

---
+++ src/services/apiService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "20731872-71a610166f6b50a9cc8e22574";
+const perPage = 12;
+
+const fetchHits = ({ searchQuery = "", currentPage = 1 }) => {
+  return axios
+    .get(
+      `${BASE_URL}?q=${searchQuery}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`
+    )
+    .then((response) => response.data.hits);
+};
+
+const apiService = {
+  fetchHits,
+};
+
+export default apiService;
+// ============= Assign object to a variable before exporting as module default
+// const exportedObject = {
+//   getAll,
+//   create,
+// };
+
+// export default exportedObject;
